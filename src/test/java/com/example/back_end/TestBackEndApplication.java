28b750c0672f88c35b9ab72b07d01c4d45d1b299
Abdallah Abu Hijleh <Abdallahabuhijleh69@gmail.com>
2025-11-29 14:18:57 +0200

    Fixed for conflicts

--- src/test/java/com/example/back_end/TestBackEndApplication.java
+++ src/test/java/com/example/back_end/TestBackEndApplication.java
@@ -1,6 +1,7 @@
 package com.example.backend;
 
 import org.springframework.boot.SpringApplication;
+import org.testcontainers.utility.TestcontainersConfiguration;
 
 public class TestBackEndApplication {
 
